07eb5bc71acb718ecb98eb869bbc00feeb17ff59
goldshoot0720 <goldshoot0720@gmail.com>
2025-08-05 00:36:49 +0800

    20250805 v1

---
+++ git-iron2038mayor.github.io.bat
@@ -0,0 +1,8 @@
+cd "C:\Users\chbon\OneDrive\Documents\iron2038mayor.github.io"
+git init
+git add .
+git commit -m "20250805 v1"
+git remote add origin https://github.com/iron2038mayor/iron2038mayor.github.io.git
+git checkout -b main
+git push -u origin  main --force
+pause
